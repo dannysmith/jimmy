078757dd688eb3ba0260df257926a9caf3e12784
Danny Smith <hi@danny.is>
2017-12-05 19:09:06 +0000

    Remove debugging controller
    Reverts 0567329d74ee70c5db5605d49db969b12b4f8a15.

--- app/controllers/debugging_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-DebuggingException = Class.new(StandardError)
-
-class DebuggingController < ApplicationController
-  def raise_exception
-    raise DebuggingException, 'You are deliberatley raising a debugger exception'
-  end
-end
