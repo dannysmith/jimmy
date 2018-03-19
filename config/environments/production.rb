ae8cfd90f137d5544e7e3067b051e3b0e00cd88a
Danny Smith <hi@danny.is>
2018-03-19 00:33:23 +0000

    Correctly configure Rails Encrypted Credentials and ActiveStorage AWS services

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -92,5 +92,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Store files on Amazon S3.
-  config.active_storage.service = :amazon
+  config.active_storage.service = if ENV['STAGING']
+                                    :amazon_staging
+                                  else
+                                    :amazon_production
+                                  end
 end
