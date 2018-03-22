7fb081f61a1a71524dfa90765a7c266b8c87172c
Danny Smith <hi@danny.is>
2018-03-22 16:30:00 +0000

    Add Helper for ERB components

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # Component Renderer
+  def component(component_name, locals = {}, &block)
+    name = component_name.split('_').first
+    render("components/#{name}/#{component_name}", locals, &block)
+  end
+  alias c component
 end
