0cc50ff5c963d01aa904d42389ec2c53ff2229d3
Danny Smith <hi@danny.is>
2018-03-22 19:01:07 +0000

    Require generators
    I'm hoping this will fix an exception in production

--- lib/generators/component.rb
+++ lib/generators/component.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'rails/generators'
+
 # Generates an ERB-based froen-end component.
 # See documentation for more details.
 class ComponentGenerator < Rails::Generators::Base
