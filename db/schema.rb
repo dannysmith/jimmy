ea92d75b4b593a363859993af77886ec2772c7f6
Danny Smith <hi@danny.is>
2018-03-22 00:06:23 +0000

    Add Password field to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_18_012450) do
+ActiveRecord::Schema.define(version: 2018_03_21_235942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2018_03_18_012450) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.hstore "metadata"
+    t.string "password_digest"
     t.index ["username"], name: "index_users_on_username"
   end
 
