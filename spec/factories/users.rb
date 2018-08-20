4e959424a9e502fd69b9b2acd32157d73613cf06
Danny Smith <hi@danny.is>
2018-08-20 23:39:30 +0100

    Upgrade Outdated Ruby Dependancies
    Recommended by Dependabot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,9 @@
 
 FactoryBot.define do
   factory :user do
-    username 'joebloggs'
-    primary_email 'jo2_bloggs@example.com'
-    name 'Joe H. Bloggs'
-    password 'password12345'
+    username { 'joebloggs' }
+    primary_email { 'jo2_bloggs@example.com' }
+    name { 'Joe H. Bloggs' }
+    password { 'password12345' }
   end
 end
